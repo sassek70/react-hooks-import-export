bba7632252d31b5bb5117d5a8efcf7a936fb9a49
Kevin Sasse <sassek70@gmail.com>
2022-10-11 16:44:22 -0400

    finished code along

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from './parks/RockyMountain'
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
+  
   return <h1>Colorado State Parks!</h1>;
 }
+
+
+
+console.log(RMFunctions.trees);
+// => "Aspen and Pine"
+
+RMFunctions.wildlife();
+// => "Elk, Bighorn Sheep, Moose"
+
+RMFunctions.elevation();
+// => Attempted import error
+
+export default ColoradoStateParks
